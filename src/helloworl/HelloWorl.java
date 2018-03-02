b36f608d88af0b5eada9030b9f721c3f03b851a3
Brittany Couts <brittanycouts@hotmail.com>
2018-03-02 12:34:51 -0700

    hello world message changed

--- src/helloworl/HelloWorl.java
+++ src/helloworl/HelloWorl.java
@@ -17,7 +17,7 @@ public class HelloWorl
      */
     public static void main(String[] args) 
     {
-       System.out.println("hello world");
+       System.out.println("hello world update");
     }
     
 }
